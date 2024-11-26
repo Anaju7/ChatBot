ec789f39fadfc82c871ecf710cd95c62987d3179
Anaju7 <anajulia.anaju7@gmail.com>
2024-11-26 20:19:21 -0300

    Primeiro commit

---
+++ Rag/rag.py
@@ -0,0 +1,8 @@
+import os
+from decouple import config
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_huggingface import HuggingFaceEmbeddings
+
+
